a99a44b30dce95c0f9027b7390f3404f76184148
Даниил <2019-00464@students.ssau.ru>
2020-10-01 09:38:26 +0300

    Добавлены модификаторы + корректировка в названиях переменных

--- src/test/java/ru/ssau/tk/Lilpank/Lab_Slaves_/functions/MathFunctionTest.java
+++ src/test/java/ru/ssau/tk/Lilpank/Lab_Slaves_/functions/MathFunctionTest.java
@@ -3,21 +3,22 @@ package ru.ssau.tk.Lilpank.Lab_Slaves_.functions;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
+
 public class MathFunctionTest {
-    MathFunction x = new IdentityFunction();
-    MathFunction sqr = new SqrFunction();
-    MathFunction one = new UnitFunction();
-    MathFunction lg = new LgFunction();
-    MathFunction _1function = (sqr).andThen(sqr).andThen(x);
-    MathFunction _2function = (lg).andThen(sqr).andThen(sqr).andThen(x);
+    private final MathFunction x = new IdentityFunction();
+    private final MathFunction sqr = new SqrFunction();
+    private final MathFunction one = new UnitFunction();
+    private final MathFunction lg = new LgFunction();
+    private final MathFunction function1 = (sqr).andThen(sqr).andThen(x);
+    private final MathFunction function2 = (lg).andThen(sqr).andThen(sqr).andThen(x);
 
 
     @Test
     public void testAndThen() {
-        assertNotEquals(_1function.apply(10000), 100);
-        assertEquals(_1function.andThen(one).apply(0), 1);
-        assertEquals(_1function.apply(10), 10000);
-        assertEquals(_2function.apply(100), 16);
+        assertNotEquals(function1.apply(10000), 100);
+        assertEquals(function1.andThen(one).apply(0), 1);
+        assertEquals(function1.apply(10), 10000);
+        assertEquals(function2.apply(100), 16);
 
     }
 }
