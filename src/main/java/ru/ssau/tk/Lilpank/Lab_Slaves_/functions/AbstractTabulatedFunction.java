06a0608b0d93b59aff34b5e9f4c669af119756d6
Победитель <nikita37pobeditel@mail.ru>
2020-10-08 20:14:08 +0400

    Исправленное 1 упражнение 2 задания 2 лабораторной.

--- src/main/java/ru/ssau/tk/Lilpank/Lab_Slaves_/functions/AbstractTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/Lilpank/Lab_Slaves_/functions/AbstractTabulatedFunction.java
@@ -3,9 +3,21 @@ package ru.ssau.tk.Lilpank.Lab_Slaves_.functions;
 public abstract class AbstractTabulatedFunction implements TabulatedFunction {
     private int count;
 
-    @Override
     public int getCount() {
         return count;
+    }
+
+    public double apply(double x) {
+        if (x < leftBound()) {
+            return extrapolateLeft(x);
+        }
+        if (x > rightBound()) {
+            return extrapolateRight(x);
+        }
+        if (indexOfX(x) != -1) {
+            return getY(getCount());
+        }
+        else interpolate(x, floorIndexOfX(x));
     }
 
     protected abstract int floorIndexOfX(double x);
