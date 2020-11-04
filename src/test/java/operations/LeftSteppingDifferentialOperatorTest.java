8224d5bc641cb4654d7356a11d40220453eafcdb
Победитель <nikita37pobeditel@mail.ru>
2020-11-05 00:33:36 +0400

    3.2. 3 пункт Y  и подготовка тестов.

---
+++ src/test/java/operations/LeftSteppingDifferentialOperatorTest.java
@@ -0,0 +1,12 @@
+package operations;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class LeftSteppingDifferentialOperatorTest {
+
+    @Test
+    public void testDerive() {
+    }
+}
--- src/test/java/operations/TabulatedFunctionOperationServiceTest.java
+++ src/test/java/operations/TabulatedFunctionOperationServiceTest.java
@@ -9,4 +9,24 @@ public class TabulatedFunctionOperationServiceTest {
     @Test
     public void testAsPoints() {
     }
+
+    @Test
+    public void testGetFactory() {
+    }
+
+    @Test
+    public void testSetFactory() {
+    }
+
+    @Test
+    public void testTestAsPoints() {
+    }
+
+    @Test
+    public void testSum() {
+    }
+
+    @Test
+    public void testSubtract() {
+    }
 }
