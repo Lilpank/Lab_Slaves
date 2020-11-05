520373933a132b84307172134e1cc1427138539d
Даниил <2019-00464@students.ssau.ru>
2020-11-05 13:43:04 +0300

    Задание 3 пункт 6

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -10,12 +10,22 @@ final public class FunctionsIO {
         throw new UnsupportedOperationException();
     }
 
-    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
         PrintWriter printWriter = new PrintWriter(writer);
         printWriter.println(function.getCount());
         for (Point temp : function) {
             printWriter.printf("%f %f\n", temp.x, temp.y);
         }
         printWriter.flush();
+    }
+
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+        for (Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+        dataOutputStream.flush();
     }
 }
--- src/main/java/io/TabulatedFunctionFileWriter.java
+++ src/main/java/io/TabulatedFunctionFileWriter.java
@@ -13,8 +13,8 @@ public class TabulatedFunctionFileWriter {
         TabulatedFunction listFunction = new LinkedListTabulatedFunction(new double[]{1.2, 2.3, 3.6}, new double[]{4, 5, 6});
         TabulatedFunction arrayFunction = new ArrayTabulatedFunction(new double[]{1, 4, 6}, new double[]{16, 25, 36});
 
-        try (BufferedWriter outArray = new BufferedWriter(new FileWriter("output/array function.txt"))) {
-            BufferedWriter outList = new BufferedWriter(new FileWriter("output/linked list function.txt"));
+        try (BufferedWriter outArray = new BufferedWriter(new FileWriter("output/array function.txt"));
+            BufferedWriter outList = new BufferedWriter(new FileWriter("output/linked list function.txt"))){
 
             FunctionsIO.writeTabulatedFunction(outList, listFunction);
             FunctionsIO.writeTabulatedFunction(outArray, arrayFunction);
