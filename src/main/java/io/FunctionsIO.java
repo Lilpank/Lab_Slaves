a6860d224e4ceaea80c50b8b464921b619596e7c
Даниил <2019-00464@students.ssau.ru>
2020-11-06 15:41:00 +0300

    Задание 3 пункт 8 вроде все так.

--- src/main/java/io/FunctionsIO.java
+++ src/main/java/io/FunctionsIO.java
@@ -49,4 +49,16 @@ final public class FunctionsIO {
         }
         return factory.create(xValues, yValues);
     }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        int count = dataInputStream.readInt();
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        for (int i = 0; i < count; i++) {
+            xValues[i] = dataInputStream.readDouble();
+            yValues[i] = dataInputStream.readDouble();
+        }
+        return factory.create(xValues, yValues);
+    }
 }
