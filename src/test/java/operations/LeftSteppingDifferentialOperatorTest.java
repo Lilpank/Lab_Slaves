412380f2fd3783aaa825b916c1968697aa3922c3
Победитель <nikita37pobeditel@mail.ru>
2020-11-06 20:48:48 +0400

    Сделанные тесты операций нахождения производной

--- src/test/java/operations/LeftSteppingDifferentialOperatorTest.java
+++ src/test/java/operations/LeftSteppingDifferentialOperatorTest.java
@@ -1,12 +1,23 @@
 package operations;
 
 import org.testng.annotations.Test;
+import ru.ssau.tk.Lilpank.Lab_Slaves_.functions.SqrFunction;
 
 import static org.testng.Assert.*;
 
+
 public class LeftSteppingDifferentialOperatorTest {
+    private final static double DELTA = 0.000001;
 
     @Test
     public void testDerive() {
+        double step = 0.001;
+
+        LeftSteppingDifferentialOperator differentialOperator = new LeftSteppingDifferentialOperator(step);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(1), 1.999, DELTA);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(2), 3.999, DELTA);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(3), 5.999, DELTA);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(4), 7.999, DELTA);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(5), 9.999, DELTA);
     }
 }
