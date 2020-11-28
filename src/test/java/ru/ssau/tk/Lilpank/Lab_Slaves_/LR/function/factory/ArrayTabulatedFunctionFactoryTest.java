08a97a8292b765c6a20c5ad42f7267841719f8ca
Даниил <2019-00464@students.ssau.ru>
2020-11-28 22:24:51 +0300

    3.2 * тест для ArrayTabulatedFunctionFactoryTest

--- src/test/java/ru/ssau/tk/Lilpank/Lab_Slaves_/LR/function/factory/ArrayTabulatedFunctionFactoryTest.java
+++ src/test/java/ru/ssau/tk/Lilpank/Lab_Slaves_/LR/function/factory/ArrayTabulatedFunctionFactoryTest.java
@@ -14,4 +14,19 @@ public class ArrayTabulatedFunctionFactoryTest {
         assertTrue(temp.create(new double[]{1, 2, 3}, new double[]{1, 2, 3}) instanceof ArrayTabulatedFunction);
         assertFalse(temp.create(new double[]{1, 2, 3}, new double[]{1, 2, 3}) instanceof ArrayTabulatedFunctionFactory);
     }
+    @Test
+    public void testTabulatedFunctionFactoryCrateStrict() {
+        ArrayTabulatedFunctionFactory tempFactory = new ArrayTabulatedFunctionFactory();
+        assertEquals(tempFactory.createStrict(new double[]{1, 2, 3}, new double[]{10, 2, 3}).getCount(),3);
+        assertTrue(tempFactory.createStrict(new double[]{1, 2, 3}, new double[]{10, 2, 3}) instanceof StrictTabulatedFunction);
+        assertThrows(UnsupportedOperationException.class, () -> tempFactory.createStrict(new double[]{1, 2, 3}, new double[]{10, 2, 3}).apply(5));
+    }
+
+    @Test
+    public void testTabulatedFunctionFactoryCreateUnmodifiable() {
+        ArrayTabulatedFunctionFactory tempFactory = new ArrayTabulatedFunctionFactory();
+        assertEquals(tempFactory.createUnmodifiable(new double[]{1, 2, 3}, new double[]{10, 2, 3}).getCount(), 3);
+        assertTrue(tempFactory.createUnmodifiable(new double[]{1, 2, 3}, new double[]{10, 2, 3}) instanceof UnmodifiableTabulatedFunction);
+        assertThrows(UnsupportedOperationException.class, () -> tempFactory.createUnmodifiable(new double[]{1, 2, 3}, new double[]{10, 2, 3}).setY(1, 5));
+    }
 }
